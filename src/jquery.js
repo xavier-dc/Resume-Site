44a9c34b6f626c648cea0b9d2081514023aacab3
xavier-dc <xdecardenas@gmail.com>
2020-12-15 10:34:34 -0500

    added transitions, hover animations to links to projects

--- src/jquery.js
+++ src/jquery.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {
     $("#name, h3, p").animate({
-        opacity: 1,
+        padding: "+8px",
+        opacity: 1
 }, 2000);
 })
 
 $(document).scroll(function () {
-var windowheight = $(window).innerHeight();
 var scrollval = $(document).scrollTop();
 
-console.log($("#about-me").innerHeight());
-console.log("scroll -> " + scrollval);
+scrollval >= 0 ? $("#about-me").animate({
+        top:"-10px",
+}, 2000) : $("#about-me").hide();
 
-scrollval >= 0 ? $("#about-me").fadeIn(1000) : $("#about-me").hide();
-scrollval >= ($("#about-me").innerHeight() - 10) ? $("#projects").fadeIn(1000) : $("#projects").hide();
+scrollval >= ($("#about-me").innerHeight() - 20) ? $("#projects").fadeIn(1000) : $("#projects").hide();
+
 scrollval >= ($("#projects").innerHeight() - 10) ? $("#technologies").show("slow") : $("#technologies").hide();
 })
