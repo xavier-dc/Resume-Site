b6639cf442ffb054e23e31ecf8ddd0f1a9cc598b
xavier-dc <xdecardenas@gmail.com>
2020-12-16 14:53:11 -0500

    increased media queries to 1000px and fixed responsiveness of drop-down menu to work on smaller screens

--- src/jquery.js
+++ src/jquery.js
@@ -3,4 +3,18 @@ $(document).ready(function() {
         padding: "+8px",
         opacity: 1
 }, 2000);
+
+$('.menu a').on("click", function() {
+    $('.menu').slideUp();
+    }
+)
+
+$('#toggle').on('click', function() {
+    if ($('.menu').is(':visible')) {
+        $('.menu').slideUp();
+    } else {
+        $('.menu').slideDown();
+    }
+})
+
 })
