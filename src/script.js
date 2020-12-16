01ca9a89696ff3aea4e1ada036039221b6ae6ec8
xavier-dc <xdecardenas@gmail.com>
2020-12-16 11:46:23 -0500

    fixed scrolling function, sections now appear utilizing IntersectionObserver

--- src/script.js
+++ src/script.js
@@ -1,3 +1,31 @@
-const intro = document.querySelector('#introduction');
+const faders = document.querySelectorAll('.fade-in');
+const slideleft = document.querySelectorAll('.from-left');
+const slideright = document.querySelectorAll('.from-right');
 
-console.log(intro);
+const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -50px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        }
+    })
+}, appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
+
+slideleft.forEach(slider => {
+    appearOnScroll.observe(slider);
+})
+
+slideright.forEach(slider => {
+    appearOnScroll.observe(slider);
+})
