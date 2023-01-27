8f8feedd5637e3fbecde1b52441dd36b98f14efd
Xavier De Cardenas <xcardenas@convene.com>
2023-01-26 22:14:24 -0500

    added aws amplify and ses email routing to use with php

--- js/mail.php
+++ js/mail.php
@@ -1,16 +1,59 @@
 <?php
 
-$name = $_POST['contactName'];
-$email = $_POST['contactEmail'];
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require './vendor/autoload.php';
+
+$senderName = $_POST['contactName'];
+$sender = $_POST['contactEmail'];
 $message = $_POST['message'];
 
-$mailheader = "From:".$name."<".$email.">\r\n";
+$recipient = 'xdecardenas@gmail.com';
 
-$recipient = "xdecardenas+website@gmail.com";
+$usernameSmtp = 'AKIA6KDWYYSZWX6PFUUM';
+$passwordSmtp = 'BLB0Fm6YOLKMT7bX5EKxvpQ3dZ2+YFOrIXR1vLvE/qx8';
 
-mail($recipient, $subject, $message, $mailheader)
-or die("Error");
+$host = 'email-smtp.us-east-1.amazonaws.com';
+$port = 587;
 
-echo"message sent";
+$subject = 'SW Portfolio Reach Out!';
+
+$mail = new PHPMailer(true);
+
+try {
+    $mail->isSMTP();
+    $mail->setFrom($sender, $senderName);
+    $mail->Username = $usernameSmtp;
+    $mail->Password = $passwordSmtp;
+    $mail->Host = $host;
+    $mail->Port = $port;
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = 'tls';
+
+    $mail->addAddress($recipient);
+    $mail->Subject = $subject;
+    $mail->Body = $message;
+    $mail->AltBody = '...';
+    $mail->Send();
+
+    echo "Email sent!", PHP_EOL;
+} catch (phpmailerException $e) {
+    echo "An error occured. {$e->errorMessage()}", PHP_EOL;
+}
+
+// $senderName = $_POST['contactName'];
+// $sender = $_POST['contactEmail'];
+// $message = $_POST['message'];
+
+// $mailheader = "From:".$senderName."<".$senderName.">\r\n";
+
+// $recipient = "xdecardenas@gmail.com";
+
+// if (mail($recipient, $subject, $message, $mailheader)){
+//     echo 'Your email has been sent!';
+// } else {
+//     echo 'Oops! Unable to send mail, please try again.';
+// }
 
 ?>
